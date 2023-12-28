5f59b4c9285f435ebe6a97375e4d151598d86aeb
Imtiyaj Sayyad <imsayyad97@gmail.com>
2023-12-28 22:55:28 +0530

    Uploading project code

---
+++ src/components/header/Header.js
@@ -0,0 +1,20 @@
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <p href="/" className="logo"><h2><b>Enter<span className="mid">10</span><span className="last">ment</span></b></h2></p>
+            </div>
+            <div className="headerRight">
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
